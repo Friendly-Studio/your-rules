5e58556396f8fa7bb3797cac6618b43944b7edc3
Yaroslav-Zozulya <my.dev.stories@gmail.com>
2023-11-20 21:56:39 +0300

    add orders

---
+++ src/api/order-ukraine/controllers/order-ukraine.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * order-ukraine controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::order-ukraine.order-ukraine');
---
+++ src/api/order-world/controllers/order-world.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * order-world controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::order-world.order-world');
