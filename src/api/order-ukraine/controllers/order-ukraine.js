7b3c59900aaecde2aa0e72c52256f4dd0e33e11b
Yaroslav-Zozulya <my.dev.stories@gmail.com>
2023-11-26 16:22:05 +0300

    add email sending

--- src/api/order-ukraine/controllers/order-ukraine.js
+++ src/api/order-ukraine/controllers/order-ukraine.js
@@ -3,47 +3,32 @@
 /**
  * order-ukraine controller
  */
+const sendEmail = require('../../../helpers/sendEmail');
 
 const { createCoreController } = require('@strapi/strapi').factories;
-const nodemailer = require('nodemailer');
 module.exports = createCoreController(
   'api::order-ukraine.order-ukraine',
   ({ strapi }) => ({
     // some custom logic for POST request
     async create(ctx) {
       const { data, meta } = await super.create(ctx);
+      const { category, product, phone, email, full_name, city } =
+        data.attributes;
 
-      const nodemailer = require('nodemailer');
-      require('dotenv').config();
-
-      const { GOOGLE_PASSWORD } = process.env;
-
-      const transporterConfig = {
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-          user: 'my.dev.stories@gmail.com',
-          pass: `${GOOGLE_PASSWORD}`,
-        },
+      const emailTemplate = {
+        from: 'YourRulesCorsets@ukr.net',
+        to: 'YourRulesCorsets@ukr.net',
+        subject: 'У вас є нове замовлення по Україні',
+        html: `<h3>Категорія: ${category}</h3>
+              <p>Ім'я товару: ${product}</p>
+              <p>Номере телефону: ${phone}</p>
+              <p>Пошта замовника: ${email}</p>
+              <p>Ім'я замовника: ${full_name}</p>
+              <p>Адрес: ${city}</p>
+              `,
       };
 
-      const transporter =
-        nodemailer.createTransport(transporterConfig);
-
-      // const email = {
-      //   from: "my.dev.stories@gmail.com",
-      //   to: "vds.reseller@gmail.com",
-      //   subject: "Тестовое сообщение",
-      //   text: "Привет, это тестовое сообщение!",
-      // };
-
-      const sendEmail = async data => {
-        transporter
-          .sendMail({ ...data, from: 'my.dev.stories@gmail.com' })
-          .then(() => console.log('Email send success'))
-          .catch(error => console.log(error.message));
-      };
+      sendEmail(emailTemplate);
 
       return { data, meta };
     },
